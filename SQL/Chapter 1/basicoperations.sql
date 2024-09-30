c7a8a12fe173317bc743507bf1dc65194415a8c6
chitresh99 <chitreshpoojary990@gmail.com>
2024-09-30 23:49:30 +0530

    SQL COMMANDS

--- SQL/Chapter 1/basicoperations.sql
+++ SQL/Chapter 1/basicoperations.sql
@@ -8,7 +8,7 @@ USE artistsdb;
 
 -- Show this database 
 
-SELECT * from artistsdb;
+SELECT * from artists;
 
 CREATE TABLE artists (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,5 +20,19 @@ CREATE TABLE artists (
 -- Insert some data 
 
 INSERT INTO artists (name,age,genre) VALUES ('Chitresh',20,'Hiphop');
+
+CREATE TABLE shows (
+show_id INT AUTO_INCREMENT PRIMARY KEY,
+show_name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE artist_shows(
+	artist_id INT,
+	show_id INT,
+	PRIMARY KEY (artist_id,course_id),
+	FOREIGN KEY (artist_id) REFERENCES artists(id),
+	FOREIGN KEY (show_id) REFERNECES shows(show_id)
+);
 
 
